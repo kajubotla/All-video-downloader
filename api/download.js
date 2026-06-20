9ef32f9be5aa22037c12a816067859baf5285e42
kajubotla <kajubotla@gmail.com>
2026-06-20 19:06:16 +0500

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,12 +1,20 @@
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).end();
-    const { url } = req.body;
     
-    const response = await fetch("https://api.cobalt.tools/api/json", {
-        method: "POST",
-        headers: { "Content-Type": "application/json", "Accept": "application/json" },
-        body: JSON.stringify({ url: url })
-    });
-    const data = await response.json();
-    res.status(200).json(data);
+    try {
+        const { url } = req.body;
+        const response = await fetch("https://api.cobalt.tools/api/json", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json", 
+                "Accept": "application/json" 
+            },
+            body: JSON.stringify({ url: url })
+        });
+        
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Server Error" });
+    }
 }
